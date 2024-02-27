e5b873b8f6c1263e6bda44349ff6f1a1153d97a5
Ricardo Antonio <chabricardo6@gmail.com>
2024-02-27 10:09:45 -0600

    imprime nombres de la fila

--- Generar_Nombre.java
+++ Generar_Nombre.java
@@ -2,7 +2,7 @@
 public class Generar_Nombre {
 
 	public void main (String [] args) {
-		System.out.print("Ricardo Antonio Chab Pool");
+		System.out.print("Ricardo Antonio Chab Pool" + "Angel Na" + "Enrique Chan" + "Guidnel Perez" + "Diego Avilez" + "Erik");
 	}
 	
 }
